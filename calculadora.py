c0bf12c5932d8cd853f4f098809a44a4f4ff61bb
tiagogt1001 <tiagogt8@gmail.com>
2024-11-05 08:43:32 +0000

    Calculadora com funções

---
+++ calculadora.py
@@ -0,0 +1,19 @@
+from unittest import removeResult
+
+
+def soma (x,y):
+    return x+y
+
+def subtracao (x,y):
+    return x-y
+
+def divisao (x,y):
+    return x/y
+
+def multiplicacao (x,y):
+    return x*y
+
+def calculadora():
+    lista_contas = []
+
+    while True:
